4b60b5b0c959bb2f73aa8c9fd6844d78739bca99
kishore <makam.kishore.991.sandhya@gmail.com>
2025-05-15 17:38:57 +0530

    commited

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 app.use(cors({
 
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173", "https://stream-chat-frontend.vercel.app"],
     credentials: true
 }))
 app.get("/", (req, res)=>{
